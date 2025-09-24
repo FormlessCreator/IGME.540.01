362fcea8086b65ea5da32db9d17898cf03ec0673
FormlessCreator <112409264+FormlessCreator@users.noreply.github.com>
2025-09-24 12:01:07 -0400

    Update Entity
    Update summary.

--- D3D11Starter-main/Entity.cpp
+++ D3D11Starter-main/Entity.cpp
@@ -1,1 +1,18 @@
 #include "Entity.h"
+#include <memory>
+
+Transform& Entity::GetTransform()
+{
+	// TODO: insert return statement here
+	return transform;
+}
+
+std::shared_ptr<Mesh> Entity::GetMesh()
+{
+	return std::shared_ptr<Mesh>();
+}
+
+void Entity::Draw()
+{
+
+}
--- D3D11Starter-main/Entity.h
+++ D3D11Starter-main/Entity.h
@@ -11,7 +11,7 @@ public:
 	void Draw();
 
 private:
-	Transform tranform;
+	Transform transform;
 	std::shared_ptr<Mesh> mesh;
 };
 
