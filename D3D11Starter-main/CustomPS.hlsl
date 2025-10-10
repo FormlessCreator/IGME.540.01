7631f63bd3283b0fd67314c08ab6adf7d7fc60f3
FormlessCreator <112409264+FormlessCreator@users.noreply.github.com>
2025-10-09 20:58:53 -0400

    Completed!
    I finished creating the custom shader pixel color information!

--- D3D11Starter-main/CustomPS.hlsl
+++ D3D11Starter-main/CustomPS.hlsl
@@ -25,6 +25,27 @@ struct VertexToPixel
 	//float4 color			: COLOR;
 };
 
+// Create a Color Change class that changes color based on the float value it is provided.
+float4 ColorChange(float4 value)
+{
+	// Create four colors.
+    float4 color1 = float4(0.95f, 0.45f, 0.65f, 0.0f);
+    float4 color2 = float4(1.00f, 0.45f, 0.45f, 0.0f);
+    float4 color3 = float4(0.75f, 0.45f, 1.00f, 0.0f);
+    float4 color4 = float4(1.00f, 0.40f, 0.75f, 0.0f);
+	
+	// Return a random value using the color that uses a cos and the value with time.
+    float4 x = color1 + color2 * cos(color3 + color4 + value + time.r);
+	
+	// Return the new color.
+    return x;
+}
+
+// Using Chris Casioli Helper code.
+float random(float2 s)
+{
+    return frac(sin(dot(s, float2(12.9898, 78.233))) * 43758.5453123);
+}
 // --------------------------------------------------------
 // The entry point (main method) for our pixel shader
 // 
@@ -40,7 +61,20 @@ float4 main(VertexToPixel input) : SV_TARGET
 	// - This color (like most values passing through the rasterizer) is 
 	//   interpolated for each pixel between the corresponding vertices 
 	//   of the triangle we're rendering
-    return colorTint;
+    // With some reference Kishimisu's tutorial: https://www.youtube.com/watch?v=f4s1h2YETNY
+    // I added my own custom code on some of the tutorial code by adding additional code 
+    // that outputs color using randomness, color changes, distance, sine, absolute value,
+    // and other calculations.
+    float randomFloat = random(float2(input.normal.x, input.normal.z));
+    float magnitudeOfUv = distance(input.uv.x, input.uv.y);
+    float4 output = ColorChange(magnitudeOfUv);
+    magnitudeOfUv = sin(magnitudeOfUv * 8 + time + randomFloat) / 8.06;
+    magnitudeOfUv =  abs(magnitudeOfUv) + frac((sin(randomFloat / 250.0f)));
+    magnitudeOfUv = 0.03 / magnitudeOfUv;
+    output *= magnitudeOfUv + colorTint;
+	
+    // Return the 
+    return output;
 	
 	// Test:
 	//return float4(input.uv, 0, 1);
